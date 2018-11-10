ce474a49956b787f3e26f64c05c2220a831f946b
jkkariuki <jkkariuki15@gmail.com>
2018-11-10 14:17:01 -0500

    update

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -7,12 +7,15 @@ import "./Nav.css";
 class Nav extends React.Component {
 
   render() {
-    const{id, style} = this.props
+    const { id, style } = this.props
     return (
-      < div id={id} className= "dropdown show" >
-        <Link style={{style}} className="btn btn-secondary  dropdown-toggle" to="/" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          <h1 style={style}>JK</h1>
-  </Link>
+      < div id={id} className="dropdown show" >
+
+        <Link style={{ style }} className="btn btn-secondary  dropdown-toggle" to="/" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <button type="button" class="btn btn-default" aria-label="Left Align">
+            <span class="glyphicon glyphicon-align-left" aria-hidden="true"></span>
+          </button>
+        </Link>
 
         <div style={{ background: "none", fontSize: "20px" }} className="dropdown-menu" aria-labelledby="dropdownMenuLink">
           <div>
@@ -34,8 +37,8 @@ class Nav extends React.Component {
               : "dropdown-item"} to="/contact">Contact</Link>
           </div>
         </div>
-  </div >
-)
+      </div >
+    )
   }
 }
 
